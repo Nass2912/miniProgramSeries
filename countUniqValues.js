d578eda6ba992193ceb775bf80fd92a770b052af
Soobratty Anas <90127694+Nass2912@users.noreply.github.com>
2024-05-01 20:42:07 +0400

    Update countUniqValues.js

--- countUniqValues.js
+++ countUniqValues.js
@@ -4,15 +4,9 @@ const countUniqueValues = (arr) => {
     let first = 0;
     let last = arr.length
     let sum = 0;
-    let samsies;
     while(first < last){
         const same = arr[first] === arr[first + 1]
-        if(same){
-            samsies = arr[first]
-            if(samsies === arr[last]){
-                sum += 1
-            }
-        }else {
+        if(!same){
             sum += 1
         }
         first ++
